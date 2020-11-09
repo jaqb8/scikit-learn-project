ed079aed6b6f8e042afb086d0938b7eb16e98a9e
Jakub Aniszewski <qba.anisz@gmail.com>
2020-11-09 13:34:42 +0100

    Cross validation example

--- load_data.py
+++ load_data.py
@@ -9,7 +9,3 @@ def load_data(csv_file):
     classes = data[data.columns[-1]]
     data.drop(data.columns[-1], axis=1, inplace=True),
     return (data, classes)
-
-# x, y = load_data('./data_csv.csv')
-# print(y)
-# print(len(labels.FEATURES))
